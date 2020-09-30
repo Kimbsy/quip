d6e678836dcec539ddd6ebcabc0cf607502c0b6b
Kimbsy <lordkimber@gmail.com>
2020-09-30 17:11:50 +0100

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject quip "1.0.1"
+(defproject quip "1.0.2"
   :description "A 2D game library based on Quil"
   :url "https://github.com/Kimbsy/quip"
   :license {:name "Eclipse Public License"
