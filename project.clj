bcaeff88fd5d8cba055fad5c977b47f732f0d61c
Kimbsy <lordkimber@gmail.com>
2020-09-30 16:56:17 +0100

    having problems with non-snapshot releases

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject quip "1.0.0"
+(defproject quip "1.0.0-SNAPSHOT"
   :description "A 2D game library based on Quil"
   :url "https://github.com/Kimbsy/quip"
   :license {:name "Eclipse Public License"
