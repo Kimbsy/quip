0d171032db63e367634f8d9251150728083d6f54
Kimbsy <lordkimber@gmail.com>
2023-05-05 19:44:16 +0100

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject quip "2.0.2"
+(defproject quip "2.0.3"
   :description "A 2D game library based on Quil"
   :url "https://github.com/Kimbsy/quip"
   :license {:name "Eclipse Public License"
@@ -6,6 +6,7 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [quil "3.1.0"]
                  [org.clojure/math.combinatorics "0.1.6"]]
+  :plugins [[lein-codox "0.10.8"]]
   :deploy-repositories [["clojars" {:url           "https://clojars.org/repo/"
                                     :username      :env/clojars_user
                                     :password      :env/clojars_pass
