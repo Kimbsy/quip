f67a30ac58ede7d151c9f8466dbf23c449ca3ef6
Dave Kimber <lordkimber@gmail.com>
2024-04-29 10:48:20 +0100

    Add basic FABRIK example game (#9)
    Still some work to do, but a great start.

---
+++ example_games/fabrik/project.clj
@@ -0,0 +1,6 @@
+(defproject fabrik "0.1.0"
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [quip "2.0.4"]]
+  :main ^:skip-aot fabrik.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
---
+++ example_games/fabrik/src/fabrik/core.clj
@@ -0,0 +1,27 @@
+(ns fabrik.core
+  (:gen-class)
+  (:require [quip.core :as qp]
+            [fabrik.scenes.level-01 :as level-01]))
+
+(defn setup
+  "The initial state of the game"
+  []
+  {})
+
+(defn init-scenes
+  "Map of scenes in the game"
+  []
+  {:level-01 (level-01/init)})
+
+;; Configure the game
+(def fabrik-game
+  (qp/game {:title          "fabrik"
+            :size           [800 600]
+            :setup          setup
+            :init-scenes-fn init-scenes
+            :current-scene  :level-01}))
+
+(defn -main
+  "Run the game"
+  [& args]
+  (qp/run fabrik-game))
