adf5b141d1cd35774e3db7708b50e1cd99ad20a1
Dave Kimber <lordkimber@gmail.com>
2024-12-15 20:32:40 +0000

    Release/4.0.0 (#58)
    * Reword github action description
    
    * Add missing img
    
    I must've relied on it being in the main quip resources at some point.
    
    * Move to `com.kimbsy` clojars group

--- example_games/fabrik/project.clj
+++ example_games/fabrik/project.clj
@@ -1,6 +1,6 @@
 (defproject fabrik "0.1.0"
   :dependencies [[org.clojure/clojure "1.11.3"]
-                 [quip "4.0.0"]]
+                 [com.kimbsy/quip "4.0.0"]]
   :main ^:skip-aot fabrik.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- example_games/mouse-controls/project.clj
+++ example_games/mouse-controls/project.clj
@@ -1,6 +1,6 @@
 (defproject mouse-controls "0.1.0"
   :dependencies [[org.clojure/clojure "1.11.3"]
-                 [quip "4.0.0"]]
+                 [com.kimbsy/quip "4.0.0"]]
   :main ^:skip-aot mouse-controls.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
