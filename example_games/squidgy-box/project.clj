a6e7ab314b92991a8660a7d4001bfdc337535bab
Kimbsy <lordkimber@gmail.com>
2024-12-16 13:00:50 +0000

    Add `quip.util/center` fnuction
    We often want to position something in the middle of the screen, often
    enough that this is going to make things easier and clearer in many
    cases.

--- example_games/squidgy-box/project.clj
+++ example_games/squidgy-box/project.clj
@@ -1,6 +1,6 @@
 (defproject squidgy-box "0.1.0"
   :dependencies [[org.clojure/clojure "1.11.3"]
-                 [com.kimbsy/quip "4.0.0"]]
+                 [com.kimbsy/quip "4.0.2"]]
   :main ^:skip-aot squidgy-box.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
