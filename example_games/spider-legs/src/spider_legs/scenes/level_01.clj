85d67ded8b0b6b27ac88d9fd3440076302801683
Kimbsy <dave@pitch.io>
2025-05-22 14:47:14 +0100

    lots more effort in the spider example

--- example_games/spider-legs/src/spider_legs/scenes/level_01.clj
+++ example_games/spider-legs/src/spider_legs/scenes/level_01.clj
@@ -7,10 +7,19 @@
 
 (def light-green [133 255 199])
 (def dark-green [41 115 115])
+(defn rand-color [] [(rand-int 255) (rand-int 255) (rand-int 255)])
+
+(def leg-color (u/hex->rgb "#8ACDEA"))
+(def foot-color (u/hex->rgb "#F84AA7"))
+(def body-color (u/hex->rgb "#43B929"))
+(def eye-color (u/hex->rgb "#18430F"))
 
 (def min-length 50)
 (def max-length 160)
 (def max-angle 45)
+
+(def ls 100)
+(def lt 60)
 
 (defn move-foot-tween-x
   [idx [fx _] [tx _]]
@@ -78,44 +87,113 @@
             spider
             movements)))
 
+(defn draw-leg
+  [ls lt [fx fy :as f] [bx by :as b] k1?]
+  (let [l (length f b)
+
+        ;; d is the unit vector from f to b
+        [dx dy :as d] [(/ (- bx fx) l)
+                       (/ (- by fy) l)]
+
+        ;; a is the distance form f to the intersection midpoint
+        a (/ (+ (* ls ls)
+                (- (* lt lt))
+                (* l l))
+             (* l 2))
+
+        ;; p is the point along the line f->b at the intersection point
+        [px py :as p] (mapv + f (mapv #(* a %) d))
+
+        ;; perp is perpendicular to p
+        perp [(- dy) dx]
+
+        ;; h is height of k above p
+        h (Math/sqrt (- (* ls ls)
+                        (* a a)))
+
+        ;; kn are the points for the knee
+        [k1x k1y :as k1] (mapv + p (mapv #(* h %) perp))
+        [k2x k2y :as k2] (mapv - p (mapv #(* h %) perp))]
+
+    ;; draw leg
+    (q/stroke leg-color)
+    (q/stroke-weight 4)
+    (if k1?
+      (do (q/line bx by k1x k1y)
+          (q/line bx by k1x k1y)
+          (q/line k1x k1y fx fy)
+          (q/line k1x k1y fx fy))
+      (do (q/line bx by k2x k2y)
+          (q/line bx by k2x k2y)
+          (q/line k2x k2y fx fy)
+          (q/line k2x k2y fx fy)))
+    (q/no-stroke)
+
+    ;; draw foot
+    (q/fill foot-color)
+    (q/ellipse fx fy 10 10)
+    
+    ;; ;; draw intersection point
+    ;; (q/ellipse px py 10 10)
+
+    ;; ;; draw knee points
+    ;; (u/fill u/red)
+    ;; (q/ellipse k1x k1y 10 10)
+    ;; (u/fill u/blue)
+    ;; (q/ellipse k2x k2y 10 10)
+    ))
+
 (defn draw-spider
-  [{[x y] :pos
+  [{[x y :as b] :pos
     :keys [feet]}]
   (q/stroke u/white)
   (q/stroke-weight 3)
   (u/fill u/white)
 
-  ;; draw body
-  (q/ellipse x y 30 30)
-
-  (doseq [{[fx fy] :pos} feet]
+  (doseq [[i {[fx fy :as f] :pos}] (zipmap (range) feet)]
     ;; draw leg
-    (q/line x y fx fy)
-    
-    ;; draw foot
-    (q/ellipse fx fy 10 10)))
+    (draw-leg ls lt f b (< i 4)))
+
+  ;; draw body
+  (q/fill body-color)
+  (q/ellipse x (- y 10) 30 30)
+
+  (u/fill eye-color)
+  (q/rect (- x 5) (- y 10) 2 4)
+  (q/rect (+ x 5) (- y 10) 2 4))
 
 (defn spider
   [pos]
-  (let [initial-offsets [[50 -100]
-                         [100 -50]
-                         [100 50]
-                         [50 100]
-                         [-50 -100]
-                         [-100 -50]
-                         [-100 50]
-                         [-50 100]]]
-    (sprite/sprite
-     :spider
-     pos
-     :update-fn update-spider
-     :draw-fn draw-spider
-     :extra {:initial-offsets initial-offsets
-             :feet (mapv (fn [offset]
-                           {:reset-offset offset
-                            :pos (mapv + pos offset)})
-                         initial-offsets)
-             :moving-feet {}})))
+  (let [initial-offsets [[100 0]
+                         [100 25]
+                         [80 50]
+                         [60 75]
+                         [-100 0]
+                         [-100 25]
+                         [-80 50]
+                         [-60 75]]
+        s (sprite/sprite
+           :spider
+           pos
+           :update-fn update-spider
+           :draw-fn draw-spider
+           :extra {:initial-offsets initial-offsets
+                   :feet (mapv (fn [offset]
+                                 {:reset-offset offset
+                                  :pos (mapv + pos offset)})
+                               initial-offsets)
+                   :moving-feet {}})]
+    (tween/add-tween
+     s
+     (tween/tween
+      :pos
+      7
+      :step-count (u/ms->frames 800)
+      :easing-fn tween/ease-in-out-sine
+      :update-fn tween/tween-y-fn
+      :yoyo? true
+      :yoyo-update-fn tween/tween-y-yoyo-fn
+      :repeat-times ##Inf))))
 
 (defn sprites
   "The initial list of sprites for this scene"
@@ -135,16 +213,40 @@
       sprite/update-state
       tween/update-state))
 
-(defn key-pressed
-  [state e]
-  (if (= :space (:key e))
-    (update state :moving? not)
+(defn clicked
+  [state {:keys [x y button] :as e}]
+  (if (= button :right)
+    (sprite/update-sprites
+     state
+     (sprite/has-group :spider)
+     (fn [spider]
+       (-> spider
+           (update :tweens (fn [tweens]
+                             (remove #(= :current-move (:tag %))
+                                     tweens)))
+           (tween/add-tween
+            (assoc (tween/tween
+                    :pos
+                    x
+                    :from-value (get-in spider [:pos 0])
+                    :update-fn tween/tween-x-fn
+                    :easing-fn tween/ease-out-quad)
+                   :tag :current-move))
+           (tween/add-tween
+            (assoc (tween/tween
+                    :pos
+                    y
+                    :from-value (get-in spider [:pos 1])
+                    :update-fn tween/tween-y-fn
+                    :easing-fn tween/ease-out-quad)
+                   :tag :current-move)))))
     state))
 
 (defn dragged
-  [{:keys [moving?] :as state}
-   {:keys [x y p-x p-y] :as e}]
-  (if (every? zero? [p-x p-y])
+  [state
+   {:keys [x y p-x p-y button] :as e}]
+  (if (or (every? zero? [p-x p-y])
+          (not= button :left))
     state
     (let [dx (- x p-x)
           dy (- y p-y)]
@@ -153,8 +255,7 @@
        (sprite/has-group :spider)
        (fn [spider]
          (-> spider
-             (update :pos #(mapv + [dx dy] %))
-             #_(update :feet #(map (partial map + [dx dy]) %))))))))
+             (update :pos #(mapv + [dx dy] %))))))))
 
 (defn init
   "Initialise this scene"
@@ -162,5 +263,5 @@
   {:sprites   (sprites)
    :draw-fn   draw-level-01!
    :update-fn update-level-01
-   :key-pressed-fns [key-pressed]
-   :mouse-dragged-fns [dragged]})
+   :mouse-dragged-fns [dragged]
+   :mouse-pressed-fns [clicked]})
